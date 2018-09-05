b01acdf98cd3d4d180417195361b95d7680cfbb9
akeno0159 <42987366+akeno0159@users.noreply.github.com>
2018-09-05 08:29:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,40 +1,60 @@
-const Discord = require("discord.js");
-
-var PREFIX = "!";
+const Discord = require('discord.js');
 
 var bot = new Discord.Client();
+var prefix = (":");
 
-bot.on("ready", function() {
-    bot.user.setGame("TutoBot, !help");
-    console.log("Le bot a bien ete connecte");
+bot.on('ready', () => {
+    bot.user.setPresence({game: { name: 'Sucer issey dans les vestiaire', type: 0} });
+    console.log("bot ready ! ");
 });
 
-bot.on("message", async function(message) {
-    if (message.author.equals(bot.user)) return;
-
-    if(!message.content.startsWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0].toLowerCase()) {
-        case "invite":
-        message.channel.send("", {
-            embed: {
-                color: 0xFF0000,
-                author: message.author.name,
-                title: '',
-                fields: [{
-                    name: "Lien d'invitation discord",
-                    value: "https://discord.gg/CsH3kj",
-                    inline: false
-                }],
-                footer: {
-                    footer: "Partager ce lien à tous vos amis !",
-                },
-            }
-        });
-        break;
+bot.on('message', message => {
+    if (message.content === "ping"){
+        message.reply("pong");
+        console.log('ping pong');
     }
+
+    if (message.content === prefix + "help"){
+        var help_embed = new Discord.RichEmbed()
+            .setColor('#D9F200')
+            .addField("commandes du bot !", "  :help : Affiche les commandes du bot ")
+            .addField("Interaction", "ping : Le bot répond pong !")
+            .setFooter("C'est tout pour le moment !")
+         message.channel.sendEmbed(help_embed);
+         //message.channel.sendMessage("Voici les commandes du bot :\n -/help pour afficher les commandes");
+         console.log("Connande help demandée !");
+    }
+
+    if (message.content === "comment va tu akeno?"){
+        random();
+
+        if (randnum == 3){
+            console-log(randnum);
+        }
+    
+        if (randnum == 1){
+            message.reply("rias a voler mai nouveau sous vetement");
+            console.log(randnum);
+        }
+
+        if (randnum == 4){
+            message.reply("hella hella hella issey ma fais un bisou");
+            console.log(randnum);
+        }
+
+        if (randnum == 2){
+            message.reply("issey me boude -_-");
+            console.log(randnum);
+        }
+
+    }    
+
 });
 
-bot.login(process.env.TOKEN);
+function random(min, max) {
+    min = Math.ceil(0);
+    max = Math.floor(4);
+    randnum = Math.floor(Math.random() * (max - min +1) + min);
+};
+
+bot.login('NDg2MzI0MzkyODcyMTE2MjU0.DnEA4Q.zSVGZie6pk_CrL5qQ9r4FHQWSH0')
