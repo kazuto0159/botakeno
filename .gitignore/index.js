f3a53799c144f5cb69818d5aec8d72aae5cb664e
akeno0159 <42987366+akeno0159@users.noreply.github.com>
2018-09-05 08:01:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,30 +1,12 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+
+var PREFIX = "!";
 
 var bot = new Discord.Client();
-var prefix = (":");
 
-bot.on('ready', () => {
-    bot.user.setPresence({game: { name: 'Sucer issey dans les vestiaire', type: 0} });
-    console.log("bot ready ! ");
-});
-
-bot.on('message', message => {
-    if (message.content === "ping"){
-        message.reply("pong");
-        console.log('ping pong');
-    }
-
-    if (message.content === prefix + "help"){
-        var help_embed = new Discord.RichEmbed()
-            .setColor('#D9F200')
-            .addField("commandes du bot !", "  :help : Affiche les commandes du bot ")
-            .addField("Interaction", "ping : Le bot répond pong !")
-            .setFooter("C'est tout pour le moment !")
-         message.channel.sendEmbed(help_embed);
-         //message.channel.sendMessage("Voici les commandes du bot :\n -/help pour afficher les commandes");
-         console.log("Connande help demandée !");
-    }
-
+bot.on("ready", function() {
+    bot.user.setGame("TutoBot, !help");
+    console.log("Le bot a bien ete connecte");
 });
 
 bot.login(process.env.TOKEN);
