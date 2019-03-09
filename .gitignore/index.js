b9df8d8fcac9b334f4fb7c8173171e86a79eef50
akeno0159 <42987366+akeno0159@users.noreply.github.com>
2019-03-09 23:21:27 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 var bot = new Discord.Client();
-var prefix = ("**");
+var prefix = ("");
 
 bot.on('ready', () => {
     bot.user.setPresence({game: { name: 'Sucer issey dans les vestiaire', type: 0} });
