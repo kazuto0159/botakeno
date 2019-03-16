3ec1c5df4d27893fd918d05a2e7a82657478d050
akeno0159 <42987366+akeno0159@users.noreply.github.com>
2019-03-16 14:50:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,6 +8,26 @@ bot.on('ready', () => {
     console.log("bot ready ! ");
 });
 
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
+    }
+
+    // You can copy/paste the actual unicode emoji in the code (not _every_ unicode emoji works)
+    receivedMessage.react("👍")
+    receivedMessage.react("🛐")
+    // Unicode emojis: https://unicode.org/emoji/charts/full-emoji-list.html
+
+    // Get every custom emoji from the server (if any) and react with each one
+    receivedMessage.guild.emojis.forEach(customEmoji => {
+        console.log(`Reacting with custom emoji: ${customEmoji.name} (${customEmoji.id})`)
+        receivedMessage.react(customEmoji)
+    })
+    // If you know the ID of the custom emoji you want, you can get it directly with:
+    // let customEmoji = receivedMessage.guild.emojis.get(emojiId)
+})
+
+
 
     
 bot.on('message', message => {
