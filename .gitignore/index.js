8dbf3d9a06fcbb44b848f5dbb51816d58a2b6591
akeno0159 <42987366+akeno0159@users.noreply.github.com>
2019-03-16 22:39:49 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -100,15 +100,12 @@ bot.on('message',message =>{
 
 bot.on('guildMemberAdd',member =>{
     member.createDM().then(channel =>{
-        return channel.send('Bienvenue Sur Mon serveur'+ member.displayName);
+        return channel.send('Bienvenue Sur Mon serveur '+ member.displayName);
+        console.log("have join" + member);
     }).catch(console.error)
 });
 
-bot.on('guildMemberRemove',member =>{
-    member.createDM().then(channel =>{
-        return channel.send('Aurevoir'+ member.displayName);
-    }).catch(console.error)
-});
+
 
 
 function random(min, max) {
